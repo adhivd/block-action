1a583ac3a8bb1a0b993a6aa5209d34bfe3ca440b
Adhiv Dhar <adhivd@gmail.com>
2018-11-18 03:57:48 -0800

    sign in, signup, admin sign in, admin sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     devise_group :user, contains: [:holder, :admin]
+
+      def configure_permitted_parameters
+        added_attrs = [:first_name, :last_name, :wallet]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+        end
 end
