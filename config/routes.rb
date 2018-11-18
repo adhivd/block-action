0ee99714efa4c58086fad21d89d06543c77f4e42
Adhiv Dhar <adhivd@gmail.com>
2018-11-18 01:36:14 -0800

    admin vs holder accounts distinguished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
-  devise_for :users
+  devise_for :holders
+  devise_for :admins
   root to: "dashboard#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
