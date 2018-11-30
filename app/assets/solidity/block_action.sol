d457c540e0ad6274d642edbdaed68bef65512690
Adhiv Dhar <adhivd@gmail.com>
2018-11-29 23:30:04 -0800

    updated smart contract

--- app/assets/solidity/block_action.sol
+++ app/assets/solidity/block_action.sol
@@ -5,10 +5,11 @@ contract BlockAction {
     //goal is to use this to store other key information (preferred payment type, etc)
     struct ClassMember {
         uint share; //out of 1 million
+        uint preferred_payment;
     }
 
     address admin; //super user the can initiate suits, trades, settlements
-    bytes32 suit;  //name of suit 
+    bytes32 suit;  //name of suit
 
 
     mapping(address => ClassMember) members;
@@ -21,26 +22,27 @@ contract BlockAction {
     }
 
     /*
-        BlockAction.at('0x4acecbe230cf637ac5f07934e2b57916bb72bc36').start_suit(["0x14723a09acff6d2a60dcdf7aa4aff308fddc160c", "0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db", "0x583031d1113ad414f02576bd6afabfb302140225"], [333333,333333,333333])
+        BlockAction.at('0x4acecbe230cf637ac5f07934e2b57916bb72bc36').start_suit(["0x14723a09acff6d2a60dcdf7aa4aff308fddc160c", "0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db", "0x583031d1113ad414f02576bd6afabfb302140225"], [333333,333333,333333], [1,1,1])
 
         Starts a suit by assigning class members shares of the settlement as defined
         by numbers less that 1 million (1/3 of the settlement = 1 million / 3)
     */
-
-    function start_suit(address[] class_members, uint[] shares) public returns (uint error_code){
+    function start_suit(address[] class_members, uint32[] shares, uint32[] preferred_payments) public returns (uint error_code){
         require (msg.sender == admin);
         require (class_members.length == shares.length);
+        require (class_members.length == preferred_payments.length);
         for (uint i = 0; i < class_members.length; i++) {
-            members[class_members[i]].share = shares[i];
+            members[class_members[i]].share = uint(shares[i]);
+            members[class_members[i]].preferred_payment = uint(preferred_payments[i]);
         }
         member_addresses = class_members;
         return 0;
     }
 
- 
+
 
     function transfer(address from, address to, uint shares_to_transfer) public returns (uint error_code) {
-        if (msg.sender != admin || msg.sender != from) return 1; //incorrect sender
+        require(msg.sender != admin || msg.sender != from); //incorrect sender
         require (members[from].share > shares_to_transfer) ; //insufficient balance
         members[to].share += shares_to_transfer;
         members[from].share -= shares_to_transfer;
